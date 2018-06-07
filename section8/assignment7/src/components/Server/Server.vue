2532d740d8db024879464aeaf1f41211736fb6f9
Adam-harte <adamharte11@hotmail.co.uk>
2018-06-07 18:20:59 +0100

    section 8 and assignemnt 7 completed for component communication

---
+++ section8/assignment7/src/components/Server/Server.vue
@@ -0,0 +1,27 @@
+<template>
+   <li class="list-group-item" @click="serverSelected">
+      Server #{{ server.id}} 
+      <h1>Status {{ server.status }}</h1>
+   </li>
+</template>
+
+<script>
+import { eventBus } from '../../main';
+
+export default {
+   props: {
+      server: Object
+   },
+   methods: {
+      serverSelected () {
+         eventBus.$emit('serverSelected', this.server)
+      }
+   }
+};
+</script>
+
+<style scoped>
+   li {
+      cursor: pointer;
+   }
+</style>
---
+++ section8/assignment7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select a server</p>
+        <p v-else>Server #{{ server.id }} selected, status: {{ server.status }}</p>
+        <hr>
+        <button @click="updateStatus">Change status</button>
+    </div>
+
+</template>
+
+<script>
+import { eventBus } from '../../main';
+
+export default {
+    data: function () {
+        return {
+            server: null
+        }
+    },
+    methods: {
+        updateStatus () {
+            this.server.status = 'Normal'
+        }
+    },
+    created () {
+        eventBus.$on('serverSelected', (server) => {
+            this.server = server;
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
