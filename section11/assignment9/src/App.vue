427053b81e086fba70ae855bf8d6c9f939b4e6a3
Adam-harte <adamharte11@hotmail.co.uk>
2018-06-15 15:00:03 +0100

    section11 completed

--- section11/assignment9/src/App.vue
+++ section11/assignment9/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!isSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -17,21 +17,68 @@
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
+                    <div class="form-group">
+                        <label for="fullName">Full Name</label>
+                        <app-full-name-input v-model="userData.fullName"></app-full-name-input>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model.lazy="userData.password">
+                                <p>{{ userData.password }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="storeData">
+                        <input
+                                type="radio"
+                                id="storeData"
+                                value="Yes"
+                                v-model="storeData"> Yes
+                    </label>
+                    <label for="dontStoreData">
+                        <input
+                                type="radio"
+                                id="dontStoreData"
+                                value="No"
+                                v-model="storeData"> No
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary" @click.prevent="submitted">Submit
+                    </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ userData.fullName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +87,28 @@
 </template>
 
 <script>
+    import FullNameInput from './FullNameInput.vue'
+
     export default {
+        data () {
+            return {
+                userData: {
+                    fullName: '',
+                    email: '',
+                    password: ''
+                },
+                storeData: 'No',
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitted () {
+                this.isSubmitted = true
+            }
+        },
+        components: {
+            appFullNameInput: FullNameInput
+        }
     }
 </script>
 
---
+++ section11/assignment9/src/FullNameInput.vue
@@ -0,0 +1,25 @@
+<template>
+   <input
+      type="text"
+      id="fullName"
+      class="form-control"
+      :value="value"
+      @input="textUpdate">
+</template>
+
+<script>
+export default {
+   methods: {
+      textUpdate (e) {
+         this.$emit('input', e.target.value)
+      }
+   },
+   props: {
+      value: String
+   }
+}
+</script>
+
+<style>
+
+</style>
