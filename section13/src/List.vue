4713019f861f5d2d6d90c80f95f558c2c71fc1c1
Adam-Harte <adamharte11@hotmail.co.uk>
2018-06-17 23:17:54 +0100

    section 13 progress up to lecture 178

---
+++ section13/src/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   import { fruitMixin } from './fruitMixin.js'
+
+    export default {
+        mixins: [fruitMixin],
+        created () {
+           console.log('Ceated hook from list component');
+        }
+    }
+</script>
+
+<style>
+
+</style>
