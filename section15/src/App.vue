711032f60172df49d5865e98077c66850bde218c
Adam-Harte <adamharte11@hotmail.co.uk>
2018-07-01 23:37:42 +0100

    section 15 progress up to lecture 217

--- section15/src/App.vue
+++ section15/src/App.vue
@@ -3,6 +3,21 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Fetch</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -10,6 +25,36 @@
 
 <script>
     export default {
+        data () {
+            user: {
+                username: '',
+                email: ''
+            },
+            users: []
+        },
+        methods: {
+            submit () {
+                this.$http.post('https://fir-demo-8cc6c.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    })
+            },
+            fetchData () {
+                this.$http.get('https://fir-demo-8cc6c.firebaseio.com/data.json')
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data) {
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    })
+            }
+        }
     }
 </script>
 
