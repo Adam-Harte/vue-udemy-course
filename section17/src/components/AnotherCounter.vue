f67627330ea002c1375968548b4a5eef064899e3
Adam-Harte <adamharte11@hotmail.co.uk>
2018-07-06 21:44:04 +0100

    section 17 progress upto lecture 265

--- section17/src/components/AnotherCounter.vue
+++ section17/src/components/AnotherCounter.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment(100)">Increment</button>
-        <button class="btn btn-primary" @click="decrement(50)">Decrement</button>
+        <button class="btn btn-primary" @click="increment({ by: 100, duration: 1000 })">Increment</button>
+        <button class="btn btn-primary" @click="decrement({ by: 50, duration: 2000  })">Decrement</button>
     </div>
 </template>
 
@@ -11,8 +11,8 @@
     export default {
         methods: {
             ...mapActions([
-                'increment',
-                'decrement'
+                'asyncIncrement',
+                'asyncDecrement'
             ])
             // increment() {
             //     this.$store.commit('increment');
