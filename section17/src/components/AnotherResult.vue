b9c8b7d75da232932a6ffd95aa0daa743f36ac07
Adam-Harte <adamharte11@hotmail.co.uk>
2018-07-06 17:10:03 +0100

    section 17 progress up to lecture 259

---
+++ section17/src/components/AnotherResult.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of clicks: {{ stringCounter }}</p>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+
+    export default {
+        computed : {
+                ...mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ])
+        }
+
+            // counter () {
+            //     return this.$store.getters.doubleCounter;
+            // },
+            // clicks () {
+            //     return this.$store.getters.stringCounter;
+            // }
+    }
+</script>
